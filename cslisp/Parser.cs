0746a0e62766a95d2a1f4e623648f5305612d0ff
HARADA Makoto <haramako@gmail.com>
2019-11-06 09:51:21 +0900

    parserを移植

---
+++ cslisp/Parser.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lisp
+{
+    public static class Cons
+    {
+        public static Value ConsWithSource(Port src, Value car, Value cdr)
+        {
+            return new Value();
+        }
+    }
+
+    public static class C
+    {
+        public static Value Nil;
+        public static Value QuasiQuote;
+        public static Value Quote;
+        public static Value Unquote;
+        public static Value UnquoteSplicing;
+    }
+
+    public class Symbol
+    {
+        public static Value Dot;
+
+        public Symbol(string ident)
+        {
+
+        }
+    }
+
+    public class Parser
+    {
+        void skipSpace(Port s)
+        {
+            for (; ; )
+            {
+                char c = s.ReadChar();
+                switch (c)
+                {
+                    case '\n':
+                    case '\r':
+                    case '\x0c':
+                    case ' ':
+                    case '\t':
+                        break;
+                    case ';':
+                        for (; ; )
+                        {
+                            c = s.ReadChar();
+                            if (c == '\n' || c == '\r' || c == '\x0c' || c == '\0' || c == -1) break;
+                        }
+                        break;
+                    default:
+                        s.UnreadChar(c);
+                        return;
+                }
+            }
+        }
+
+        Value parseList(Port s)
+        {
+            skipSpace(s);
+            char c, err;
+            switch (c = s.ReadChar())
+            {
+                case ')':
+                case ']':
+                case '\0':
+                    s.UnreadChar(c);
+                    return C.Nil;
+                default:
+                    s.UnreadChar(c);
+                    var val = parse(s);
+
+                    if (val == Symbol.Dot)
+                    {
+                        var cdr = parse(s);
+                        skipSpace(s);
+                        return cdr;
+                    }
+                    else
+                    {
+                        var cdr = parseList(s);
+                        return Cons.ConsWithSource(s, val, cdr);
+                    }
+            }
+        }
+
+        static bool _is_val_char(int c)
+        {
+            switch (c)
+            {
+                case ' ':
+                case '\t':
+                case '\n':
+                case '\r':
+                case '\x0c':
+                case '(':
+                case ')':
+                case '[':
+                case ']':
+                case '\0':
+                case -1:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        static string readToken(Port s)
+        {
+            var sb = new StringBuilder();
+            char c;
+            int i = 0;
+            for (; ; )
+            {
+                c = s.ReadChar();
+                if (!_is_val_char(c)) break;
+                sb.Append(c);
+            }
+            s.UnreadChar(c);
+            return sb.ToString();
+        }
+
+        static bool isnumber(char c)
+        {
+            return (c >= '0' && c <= 9);
+        }
+
+        static Value parseToken(string str)
+        {
+            if (isnumber(str[0]) || (str[0] == '-' && isnumber(str[1])))
+            {
+                for (int i=0; i<str.Length; i++)
+                {
+                    if (!isnumber(str[i]))
+                    {
+                        return new Value(new Symbol(str));
+                    }
+                }
+                return new Value(int.Parse(str));
+            }
+            else
+            {
+                return new Value(new Symbol(str));
+            }
+        }
+
+        char unescapeChar(Port s)
+        {
+            char c = s.ReadChar();
+            //char buf[9];
+            switch (c)
+            {
+                case '"':
+                    return '"';
+                case '\\':
+                    return '\\';
+                case 'n':
+                    return '\n';
+                case 'r':
+                    return '\r';
+                case 'f':
+                    return '\f';
+                case 't':
+                    return '\t';
+                case '0':
+                    return '\0';
+#if false
+                case 'x':
+                    stream_read(s, buf, 2);
+                    buf[2] = '\0';
+                    sscanf(buf, "%x", &c);
+                    return c;
+                case 'u':
+                    stream_read(s, buf, 4);
+                    buf[4] = '\0';
+                    sscanf(buf, "%x", &c);
+                    return c;
+                case 'U':
+                    stream_read(s, buf, 8);
+                    buf[8] = '\0';
+                    sscanf(buf, "%x", &c);
+                    return c;
+                case ' ':
+                case '\n':
+                case '\r':
+                    {
+                        bool feeded = false;
+                        for (; ; )
+                        {
+                            if (c == '\n')
+                            {
+                                if (feeded) assert(0);
+                                feeded = true;
+                            }
+                            else if (c == ' ' || c == '\f' || c == '\r' || c == '\t')
+                            {
+                                // skip
+                            }
+                            else
+                            {
+                                if (!feeded) assert(0);
+                                stream_ungetc(c, s);
+                                break;
+                            }
+                            c = stream_getc(s);
+                        }
+                        return '\n';
+                    }
+#endif
+                default:
+                    throw new LuaException($"unkonwn escaped string {c}\n");
+            }
+        }
+
+        Value readString(Port s)
+        {
+            var sb = new StringBuilder();
+            int i = 0;
+            for (; ; )
+            {
+                char c = s.ReadChar();
+                switch (c)
+                {
+                    case '"':
+                        return new Value(sb.ToString());
+                    case '\\':
+                        c = unescapeChar(s);
+                        break;
+                    case '\n':
+                    case '\r':
+                        throw new Exception();
+                    case '\0':
+                        throw new Exception();
+                }
+                sb.Append(c);
+            }
+        }
+
+        Value parse(Port s)
+        {
+            skipSpace(s);
+            char c;
+            switch (c = s.ReadChar())
+            {
+                case '\0':
+                    return C.Nil;
+                case '(':
+                case '[':
+                    {
+                        var result = parseList(s);
+                        var c2 = s.ReadChar();
+                        if (c == '(' && c2 != ')') throw new Exception();
+                        if (c == '[' && c2 != ']') throw new Exception();
+                        return result;
+                    }
+                case ')':
+                case ']':
+                    throw new LuaException("paren not matched");
+                case '#':
+                    switch (c = s.ReadChar())
+                    {
+                        case 't':
+                            return new Value(true);
+                        case 'f':
+                            return new Value(false);
+                        case ';':
+                            // s-exp comment
+                            parse(s); // skip
+                            return parse(s);
+                        default:
+                            throw new LuaException($"invalid #char {c}\n");
+                    }
+                case '"':
+                    return readString(s);
+                case '\'':
+                    {
+                        var result = parse(s);
+                        result = Cons.ConsWithSource(s, C.Quote, Cons.ConsWithSource(s, result, Value.Nil));
+                        return result;
+                    }
+                case '`':
+                    {
+                        var result = parse(s);
+                        result = Cons.ConsWithSource(s, C.QuasiQuote, Cons.ConsWithSource(s, result, Value.Nil));
+                        return result;
+                    }
+                case ',':
+                    {
+                        Value sym = C.Unquote;
+                        if ((c = s.ReadChar()) == '@')
+                        {
+                            sym = C.UnquoteSplicing;
+                        }
+                        else
+                        {
+                            s.UnreadChar(c);
+                        }
+
+                        var result = parse(s);
+                        result = Cons.ConsWithSource(s, sym, Cons.ConsWithSource(s, result, Value.Nil));
+                        return result;
+                    }
+                default:
+                    {
+                        s.UnreadChar(c);
+                        var buf = readToken(s);
+                        return parseToken(buf);
+                    }
+            }
+            throw new Exception("Must not reach");
+        }
+    }
+}
---
+++ cslisp/Port.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lisp
+{
+    public class Port
+    {
+        public char ReadChar()
+        {
+            return '\0';
+        }
+
+        public void UnreadChar(char c)
+        {
+
+        }
+
+    }
+}
